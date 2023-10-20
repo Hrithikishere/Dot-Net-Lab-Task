198286b85506cc0bee86ddde0802e3d4085e51e7
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2023-10-20 20:46:04 +0600

    Task One Completed
    Task One Completed from Lab

---
+++ TaskOne/TaskOne/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskOne.EF;
+
+namespace TaskOne.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int Id)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Categories.Find(Id);
+            var datas = (
+                from d in db.Categories
+                where d.Id == Id
+                select d).SingleOrDefault();
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category category)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Categories.Find(category.Id);
+            data.Name = category.Name;
+            
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Categories.Find(Id);
+            db.Categories.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Details()
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Categories.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ TaskOne/TaskOne/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskOne.EF;
+
+namespace TaskOne.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int Id)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Products.Find(Id);
+            var datas = (
+                from d in db.Products
+                where d.Id == Id
+                select d).SingleOrDefault();
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Product product)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Products.Find(product.Id);
+            data.Name = product.Name;
+            data.Price = product.Price;
+            data.CategoryId = product.CategoryId;
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Products.Find(Id);
+            db.Products.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Details()
+        {
+            TaskOneEntities db = new TaskOneEntities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+    }
+}
